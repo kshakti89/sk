fe2511d90ce89e4647e7923892088b6219fff6e8
unknown <kshakti.89@gmail.com>
2019-01-14 20:38:07 +0530

    changed nm

--- nm.java
+++ nm.java
@@ -8,17 +8,8 @@ class hello {
     public static void main(String[] args)
     {
     	//getEvenString("It is pleasent");
-    	String s ="GeeksForGeeks";
-    	int count[] = new int[128];
-    	for(int i=0;i<s.length();i++) {
-    		count[s.charAt(i)]++;
-    	}
     	
-    	for(int i=0;i<s.length();i++) {
-    		if (count[s.charAt(i)]==1) {
-    			System.out.println(s.charAt(i)+" is the 1st non-repititive digit in the given String");
-    			break;
-    		}
-    	}
+    			System.out.println("is the 1st non-repititive digit in the given String");
+    	
     }
 }
